3f7e7d37a7fcab4f50a9163befba5deda26a461e
MINGZI\mingz <yang.mingzi0528@gmail.com>
2024-02-08 20:19:24 +0800

    add more suppressing checks

--- main.tf
+++ main.tf
@@ -1,8 +1,10 @@
 resource "aws_s3_bucket" "example" {
   #checkov:skip=CKV_AWS_145:Ensure that S3 buckets are encrypted with KMS by default
   #checkov:skip=CKV2_AWS_62:Ensure S3 buckets should have event notifications enabled
-  #checkov:skip=Ensure that an S3 bucket has a lifecycle configuration
+  #checkov:skip=CKV2_AWS_61:Ensure that an S3 bucket has a lifecycle configuration
   #checkov:skip=CKV_AWS_144:Ensure that S3 bucket has cross-region replication enabled
   #checkov:skip=CKV_AWS_21:Ensure all data stored in the S3 bucket have versioning enabled
+  #checkov:skip=CKV_AWS_18:Ensure the S3 bucket has access logging enabled
+  #checkov:skip=CKV2_AWS_6:Ensure that S3 bucket has a Public Access block
   bucket = "mingzi-terraform-ci-bucket"
 }
